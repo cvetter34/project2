0981cbb8b21acece0eda4e7d0cc703dd8e6feb7a
Christian Vetter <cvetter34@gmail.com>
2014-03-12 17:06:11 +0800

    Added encryption parameters (encrypt_password).

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,20 @@ require 'bcrypt'
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
+  attr_accessor :password, :password_confirmation
+
+  before_save :encrypt_password
 
   field :email, type: String
   field :salt, type: String
   field :fish, type: String
+
+  protected
+
+  def encrypt_password
+    if password.present?
+      self.salt = BCrypt::Engine.generate_salt
+      self.fish = BCrypt::Engine.generate_fish(password, self.salt)
+  end
+
 end
