071a35e12e97d53bf7d9dab22869f981879810c2
Christian Vetter <cvetter34@gmail.com>
2014-03-18 16:57:09 +0800

    Add Mail error catching.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,28 +1,42 @@
 class SessionController < ApplicationController
 
-  # skip_before_action :veryify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   def new
     # @messages = flash.inspect
-    # @messages = flash.map {|key, value| "#{key.capitalize}: #{value}"}.join(";")
-    # render text: "Display the login form."
+    # @messages = flash.map {| key, value| "#{key.capitalize}: #{value}"}.join(";")
+    # render text: "Display the log in form."
     redirect_to root_url, notice: "You are logged in." if current_user
   end
 
   def create
-    # render text: "Log #{params[:user][:email]} in with #{params[:user][:email]}."
     user = User.find_by(email: params[:user][:email])
     password = params[:user][:password]
 
-    if user and password.blank?
-      user.set_password_reset
-      UserNotifier.reset_password(user).deliver
-      flash.now[:notice] = "We'll send you an email."
-      render :new
-    elsif user and user.authenticate(password)
-      session[:user_id] = user.id
-      redirect_to root_url
+    if user
+      # password reset
+      if password.blank?
+        if user.set_password_reset
+
+          begin
+            UserNotifier.reset_password(user).deliver
+            flash.now[:notice] = "We'll send you an email with instrutions for resetting"
+          rescue
+            flash.now[:alert] = "Unable to send email. Please notify webmaster"
+          end
+
+        else
+          flash.now[:alert] = "Password reset failed."
+        end
+          render :new
+      elsif user.authenticate(password)
+        #user authentication
+        session[:user_id] = user.id
+        redirect_to root_url
+      end
+
     else
+      # failure
       flash.now[:alert] = "Unable to log you in. Please check your email and password and try again."
       render :new
     end
