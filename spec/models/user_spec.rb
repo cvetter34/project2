822fc128c53fcdcb7452cee1ca6ccd982bc1c647
Christian Vetter <cvetter34@gmail.com>
2014-03-27 15:38:54 +0800

    Adding testing functionality.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe User do
+  it "is valid with an email" do
+    user = User.new(
+      email: 'cvetter34@gmail.com'
+    )
+    expect(user).to be_valid
+    # expect(true).to be_true
+  end
+
+  it "is invalid without an email" do
+    user = User.new
+    expect(user).to be_invalid
+  end
+
+  it "is invalid without a salt"
+
+  it "is invalid without a fish"
+
+end
